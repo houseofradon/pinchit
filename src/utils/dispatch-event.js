90f837dda817c6d225db84f1bca25d65e1009bd5
Philip Knape <philip.knape@gmail.com>
2017-02-10 00:44:52 +0100

    start adding func for dispatch events

---
+++ src/utils/dispatch-event.js
@@ -0,0 +1,10 @@
+/**
+ * dispatch custom events
+ *
+ * @param  {element} el         slideshow element
+ * @param  {string}  type       custom event name
+ * @param  {object}  detail     custom detail information
+ */
+export default (target, type, detail) => {
+  
+};
