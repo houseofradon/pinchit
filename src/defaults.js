ca37aa21bf0a76ed7d4f3a01ff1810a53738ccea
Philip Knape <philip.knape@gmail.com>
2017-02-10 00:43:11 +0100

    basic default props for pinchit

---
+++ src/defaults.js
@@ -0,0 +1,27 @@
+export default {
+  /**
+   * max scale a node can reach
+   * @maxPinch {Number}
+   */
+  maxPinch: 3,
+
+  /**
+   * min scale a node can reach
+   * @minPinch {Number}
+   */
+  minPinch: 300,
+
+  /**
+   * time for the snapBack of the pinch if the node has reach above
+   * or below its pinch value
+   * @snapBackSpeed {Number}
+   */
+  snapBackSpeed: 200,
+
+  /**
+   * Basic easing functions: https://developer.mozilla.org/de/docs/Web/CSS/transition-timing-function
+   * cubic bezier easing functions: http://easings.net/de
+   * @ease {String}
+   */
+  ease: 'ease',
+};
