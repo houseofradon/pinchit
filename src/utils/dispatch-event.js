f8d2e786a37a20e76540357bd4c7506aff741a51
Philip Knape <philip.knape@gmail.com>
2017-02-10 10:46:14 +0100

    removed code for linting purpose

--- src/utils/dispatch-event.js
+++ src/utils/dispatch-event.js
@@ -1,10 +1,10 @@
+// @flow
+
 /**
  * dispatch custom events
  *
- * @param  {element} el         slideshow element
+ * @param  {element} el         image element
  * @param  {string}  type       custom event name
  * @param  {object}  detail     custom detail information
  */
-export default (target, type, detail) => {
-  
-};
+export default () => {};
