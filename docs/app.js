d9c8e0f426e1a949fb2359ae9708c4ca5977d10d
Philip Knape <philip.knape@gmail.com>
2017-02-13 15:50:45 +0100

    updates doc to show that reset extends options

--- docs/app.js
+++ docs/app.js
@@ -9,10 +9,11 @@ pinchIt('.example-tree img', {
   maxScale: 4,
   minScale: 0.5,
   snapBackSpeed: 500,
-
 });
 
-const pinchImage = pinchIt('.example-four img');
+const pinchImage = pinchIt('.example-four img', {
+  snapBackSpeed: 1000,
+});
 document
   .querySelector('.reset-button')
   .addEventListener('click', () => {
