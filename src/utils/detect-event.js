848f34f7a2888142882994115ea0c9113d0185bd
Philip Knape <philip.knape@gmail.com>
2017-02-13 23:28:07 +0100

    utility function to check if we double tap the image

---
+++ src/utils/detect-event.js
@@ -0,0 +1,23 @@
+// @flow
+
+import { cancelEvent } from './handle-events';
+
+let lastTouchStart = 0;
+
+export const detectDoubleTap = (e: TouchEvent): boolean => {
+  const time = (new Date()).getTime();
+
+  if (e.touches.length > 1) {
+    lastTouchStart = 0;
+  }
+
+  if (time - lastTouchStart < 300) {
+    cancelEvent(event);
+    return true;
+  }
+
+  if (e.touches.length === 1) {
+    lastTouchStart = time;
+  }
+  return false;
+};
