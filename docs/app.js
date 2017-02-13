eb732a667d26e01910853091baf6174c34943eb9
Philip Knape <philip.knape@gmail.com>
2017-02-13 15:01:45 +0100

    updates docs

--- docs/app.js
+++ docs/app.js
@@ -4,3 +4,17 @@ import 'prismjs/themes/prism.css';
 import pinchIt from '../src/';
 
 pinchIt('.example-one img');
+pinchIt('.example-two img');
+pinchIt('.example-tree img', {
+  maxScale: 4,
+  minScale: 0.5,
+  snapBackSpeed: 500,
+
+});
+
+const pinchImage = pinchIt('.example-four img');
+document
+  .querySelector('.reset-button')
+  .addEventListener('click', () => {
+    pinchImage.reset();
+  });
