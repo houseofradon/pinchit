0336301c3db6a2857508127761b6ed7c93c9e610
Philip Knape <philip.knape@gmail.com>
2017-02-15 13:48:14 +0100

    adds flow anotations

--- src/utils/handle-event.js
+++ src/utils/handle-event.js
@@ -1,5 +1,15 @@
 // @flow
 
+type TochPage = {
+  pageX: number;
+  pageY: number;
+}
+
+type Touch = {
+  x: number;
+  y: number;
+};
+
 /**
  *  cancelEvent - Cancel Events so we dont bubble up our events to the document
  *
@@ -11,25 +21,25 @@ export const cancelEvent = (e: Event): void => {
   e.preventDefault();
 };
 
- /**
-  * Returns the touches of an event relative to the container offset
-  * @param event
-  * @return array touches
-  */
-export const getTouches = (touches: Array<Object>) => {
+/**
+ * Returns the touches of an event relative to the container offset
+ * @param event
+ * @return array touches
+ */
+export const getTouches = (touches: Array<TochPage>): Array<Touch> => {
   return touches.map(touch => ({
     x: touch.pageX,
     y: touch.pageY,
   }));
 };
 
- /**
-  * getDistance - Calculate the distance between our fingers
-  *
-  * @param { Array } touches touches passas an array from TouchList
-  * @return { Number } the calcualted distance between the fingers
-  **/
-export const getDistance = (touches: Array<Object>): number => {
+/**
+ * getDistance - Calculate the distance between our fingers
+ *
+ * @param { Array } touches touches passas an array from TouchList
+ * @return { Number } the calcualted distance between the fingers
+ **/
+export const getDistance = (touches: Array<Touch>): number => {
   const [first, second] = touches;
   return Math.sqrt(
     ((first.x - second.x) * (first.x - second.x)) +
