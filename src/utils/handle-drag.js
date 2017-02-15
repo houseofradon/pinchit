f32bc1f828d49ace3868c876d2b544c84c67fd88
Philip Knape <philip.knape@gmail.com>
2017-02-15 12:31:37 +0100

    adds sanitzeoffset helper function and removes unused detectedge function

--- src/utils/handle-drag.js
+++ src/utils/handle-drag.js
@@ -1,8 +1,24 @@
 // @flow
 
+import { getParentX, getParentY } from './handle-pinch';
+
 type Center = {
   x: number;
   y: number;
+};
+
+export const sanitizeOffset = (el: EventTarget, offset: Center, zoomFactor: number): Object => {
+  const maxX = (zoomFactor - 1) * getParentX(el);
+  const maxY = (zoomFactor - 1) * getParentY(el);
+  const maxOffsetX = Math.max(maxX, 0);
+  const maxOffsetY = Math.max(maxY, 0);
+  const minOffsetX = Math.min(maxX, 0);
+  const minOffsetY = Math.min(maxY, 0);
+
+  return {
+    x: Math.min(Math.max(offset.x, minOffsetX), maxOffsetX),
+    y: Math.min(Math.max(offset.y, minOffsetY), maxOffsetY)
+  };
 };
 
 export const drag = (center: Center, lastCenter: Center, lastOffset: Object) => (
@@ -11,5 +27,3 @@ export const drag = (center: Center, lastCenter: Center, lastOffset: Object) => 
     y: lastOffset.y + -(center.y - lastCenter.y)
   } : lastOffset
 );
-
-export const detectEdges = () => {}
