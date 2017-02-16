b55540b6a062ec0a39b50cc74ae7b1154d0811de
Philip Knape <philip.knape@gmail.com>
2017-02-16 08:24:15 +0100

    adds linting

--- src/utils/dispatch-event.js
+++ src/utils/dispatch-event.js
@@ -5,46 +5,46 @@
  */
 
 const eventDispatcher = () => {
-   const events = {};
+  const events = {};
 
-   /**
-    * Register a handler for event, to be fired
-    * for every event.
-    */
-   const on = (eventName: string, handler: Function) => {
-     events[eventName] = events[eventName] || [];
-     events[eventName].push(handler);
-     return this;
-   };
+  /**
+   * Register a handler for event, to be fired
+   * for every event.
+   */
+  const on = (eventName: string, handler: Function) => {
+    events[eventName] = events[eventName] || [];
+    events[eventName].push(handler);
+    return this;
+  };
 
-   /**
+  /**
    * Deregister a handler for event.
    */
-   const off = (eventName: string, handler: Function) => {
-     if (events[eventName]) {
-       for (let i = 0; i < events[eventName].length; i += 1) {
-         if (events[eventName][i] === handler) {
-           events[eventName].splice(i, 1);
-           break;
-         }
-       }
-     }
-   };
+  const off = (eventName: string, handler: Function) => {
+    if (events[eventName]) {
+      for (let i = 0; i < events[eventName].length; i += 1) {
+        if (events[eventName][i] === handler) {
+          events[eventName].splice(i, 1);
+          break;
+        }
+      }
+    }
+  };
 
-   const dispatch = (eventName: string, data: Object) => {
-     if (events[eventName]) {
-       events[eventName].forEach((fn) => {
-         fn(data);
-       });
-     }
-   };
+  const dispatch = (eventName: string, data: Object) => {
+    if (events[eventName]) {
+      events[eventName].forEach((fn) => {
+        fn(data);
+      });
+    }
+  };
 
-   return {
-     on,
-     off,
-     dispatch,
-   };
- };
+  return {
+    on,
+    off,
+    dispatch,
+  };
+};
 
 
-export default eventDispatcher
+export default eventDispatcher;
